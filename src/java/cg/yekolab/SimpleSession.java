be20b35bc3bb7fbbe172342cf3c0231753906c27
orionwambert <wambertorion@gmail.com>
2017-10-27 17:30:17 +0100

    Mise ne place d'une session tracking basique

---
+++ src/java/cg/yekolab/SimpleSession.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cg.yekolab;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author HP Notebook
+ */
+@WebServlet(value = "/")
+public class SimpleSession extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SimpleSession</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SimpleSession at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        /**
+         * Recuperation de la session de la requete
+         * request:coté serveur
+         * response:coté client
+         */
+        HttpSession session=request.getSession();
+        /**
+         * 
+         * Lié un objet à une session (la session en cours)
+         */
+        session.setAttribute("session", "12");
+        
+        session.getAttribute("session");
+        
+        
+        
+        
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
